6902e7ba058e68b4d1c71cce2f49e267b797972a
johngoeltz <80011540+johngoeltz@users.noreply.github.com>
2021-03-04 09:57:39 -0800

    add description to catbind() function

--- pypkgs_jg/pypkgs_jg.py
+++ pypkgs_jg/pypkgs_jg.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def catbind(a, b):
+    # concatenate categorical series in pandas
     concatenated = pd.concat([pd.Series(a.astype("str")),
                               pd.Series(b.astype("str"))])
     return pd.Categorical(concatenated)
